195a24b303ace215e37642c18451acbd32cbc234
ryosuke <ryosukekt89@gmail.com>
2015-11-09 17:37:57 +0900

    php_kiso1_kadai1-6.php(課題1-6)を投稿しました

---
+++ php_kiso1_kadai1-6.php
@@ -0,0 +1,67 @@
+<?php
+
+//課題1
+
+echo 'Hello world';
+
+echo '<br>';
+
+//課題2
+
+echo 'groove' . '-' . 'gear';
+
+echo '<br>';
+
+//課題3
+
+echo 'My name is Ryosuke Kato. ';
+
+echo 'I am 26 years old. ';
+
+echo 'My hobby is badminton.';
+
+echo '<br>';
+
+//課題4,5
+
+//変数
+
+$hensuu = 10;
+
+//定数
+
+const teisuu = 4;
+
+echo $hensuu + teisuu;
+
+echo '<br>';
+
+echo $hensuu - teisuu;
+
+echo '<br>';
+
+echo $hensuu * teisuu;
+
+echo '<br>';
+
+echo $hensuu / teisuu;
+
+echo '<br>';
+
+echo $hensuu % teisuu;
+
+echo '<br>';
+
+//課題6
+
+$param = 1;
+
+if ($param == 1) {echo '1です！';
+
+} elseif ($param == 2) {echo 'プログラミングキャンプ！';
+
+} elseif ($param == 'a') {echo '文字です！';
+
+} else {echo 'その他です！';
+
+};
